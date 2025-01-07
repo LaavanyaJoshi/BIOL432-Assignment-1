a487b6a961f308006860b4f8f5c25cd1de21e189
Laavanya Joshi <laavanya.joshi@hotmail.com>
2025-01-07 09:57:31 -0500

    Updated with csv file

--- volumeEstimato.R
+++ volumeEstimato.R
@@ -6,3 +6,7 @@ measurements <- read.csv("C:/Users/laava/OneDrive/Documents/Queen's University/F
 measurements <- measurements %>%
   mutate(Limb_volume = pi * Limb_width^2 * Limb_length) # adds a column for volume
 
+
+write.csv(data1, "measurements.csv", row.names = F) #creates the csv file
+write.csv(data1, "C:/Users/laava/OneDrive/Documents/Queen's University/Fourth Year/BIOL 432/BIOL432-Assignment-1/measurements.csv", row.names = F) #sends the csv file to the specified location
+
