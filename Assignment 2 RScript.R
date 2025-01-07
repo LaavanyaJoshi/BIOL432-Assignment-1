7de258659693f720883e4447070c36caafc973d1
Laavanya Joshi <laavanya.joshi@hotmail.com>
2025-01-07 09:13:41 -0500

    Added a column for limb volume calculations

---
+++ Assignment 2 RScript.R
@@ -0,0 +1,15 @@
+
+library(dplyr)
+
+measurements <- read.csv("C:/Users/laava/OneDrive/Documents/Queen's University/Fourth Year/BIOL 432/BIOL432-Assignment-1/measurements.csv") #opens the file
+
+measurements <- measurements %>%
+  mutate(Limb_volume = pi * Limb_width^2 * Limb_length) # adds a column for volume
+
+
+
+
+
+
+
+
