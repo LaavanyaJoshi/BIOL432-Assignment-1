2a8c297fb7a43d7528090e01201f046c5efabe03
Laavanya Joshi <laavanya.joshi@hotmail.com>
2025-01-07 21:41:57 -0500

    Updated cylinder formula

--- volumeEstimato.R
+++ volumeEstimato.R
@@ -4,9 +4,9 @@ library(dplyr)
 # Gets the csv file
 measurements <- read.csv("C:/Users/laava/OneDrive/Documents/Queen's University/Fourth Year/BIOL 432/BIOL432-Assignment-1/measurements.csv") #opens the file
 
-# Adds a column for volume
+# Adds a column for volume, using the basic formula for a cylinder
 measurements <- measurements %>%
-  mutate(Limb_volume = pi * Limb_width^2 * Limb_length)
+  mutate(Limb_volume = pi * (Limb_width / 2)^2 * Limb_length)
 
 #Updates the csv file in the computer
 write.csv(measurements, "measurements.csv", row.names = F)
